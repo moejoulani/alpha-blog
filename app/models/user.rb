25d39de76e28f053b5928807e0ed77846916e968
Mohammad AlJoulani <mjoulani97@gmail.com>
2022-07-28 12:56:55 +0300

    Add Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-
+ validates(:username,presence: true,uniqueness:{case_sensitive: false} ,length: {minimum: 3,maximum:  25})
+VALID_EMAIL_REGEX = /\A(\S+)@(.+)\.(\S+)\z/
+ validates(:email, presence: true, uniqueness: true ,format: {}, length: {maximum: 105} ,format: { with: VALID_EMAIL_REGEX , message: 'Email Invalid HOHA'})
 end
