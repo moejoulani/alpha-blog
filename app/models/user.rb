4f4fdc18ccb69579c70076a759b6090d1abc2176
Mohammad AlJoulani <mjoulani97@gmail.com>
2022-07-28 16:11:29 +0300

    Add Secure Password Using BCrype Gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
    has_many(:articles)
-   
+   before_save{ self.email = email.downcase}
+
  validates(:username,presence: true,uniqueness:{case_sensitive: false} ,length: {minimum: 3,maximum:  25})
 VALID_EMAIL_REGEX = /\A(\S+)@(.+)\.(\S+)\z/
  validates(:email, presence: true, uniqueness: true ,format: {}, length: {maximum: 105} ,format: { with: VALID_EMAIL_REGEX , message: 'Email Invalid HOHA'})
 
-
+has_secure_password
 
 end
